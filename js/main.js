cc42a4c07511d4e93b0036f6112f0afcf750f01d
Maximiliano Castro <max@axiomzen.co>
2015-07-27 22:33:55 -0300

    carousel slider

--- js/main.js
+++ js/main.js
@@ -10,25 +10,42 @@
   var $document = $(document);
 
   $document.ready(function() {
-    $document.on('click', 'button', function() {
-      var $button = $(this),
-          $count = $('#count');
-
-      $.post('/count/increment', function(res) {
-        if (res.newCount >= 4) {
-          $count.text(42);
-          $button.remove();
-
-          var img = $('<img>', {
-            src: 'img/goat.jpg'
-          });
-
-          $('body').append(img);
-        } else {
-          $count.text(res.newCount);
+    $('.slick-carousel').slick({
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 300,
+      autoplay: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-
-      });
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
   });
 
