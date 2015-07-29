147de6921c73b427823401cf2e432a2f485cc141
Maximiliano Castro <max@axiomzen.co>
2015-07-29 19:09:59 -0300

    render every jade page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,10 @@ var Routes = function(app) {
   app.get('/', function(req, res) {
     res.render('index.jade', {});
   });
+
+  app.get('/*', function(req, res) {
+    res.render(req.params[0] + '.jade', {});
+  });
 };
 
 module.exports = Routes;
