553e542b31ac62a987d4e58ec01756584d649c0f
Maximiliano Castro <max@axiomzen.co>
2015-08-17 00:14:05 -0300

    Add contact form for product page

--- js/products.js
+++ js/products.js
@@ -8,12 +8,21 @@
   $document.ready(function() {
     var $productsInfo = $('.product-info');
     var $productsList = $('.product');
+    var $form = $('.cotizar-form');
+    var $active = $productsInfo.filter('[data-index="0"]') ;
     $document.on('click', '.product', function() {
       var $this = $(this);
       $productsList.removeClass('active');
       $this.addClass('active');
       $('.product-info').addClass('hidden');
-      $productsInfo.filter('[data-index="' +  $this.data('index') + '"]').removeClass('hidden');
+      $active = $productsInfo.filter('[data-index="' +  $this.data('index') + '"]');
+      $active.removeClass('hidden');
+      $form.addClass('hidden');
+      $active.append($form);
+    });
+    $document.on('click', '.cotizar', function() {
+      $form.removeClass('hidden');
+      $form[0].reset();
     });
   });
 })();
