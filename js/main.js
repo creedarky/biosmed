3fdbdbd7ad3f0aae2db0198a1821e9ca400c328c
Maximiliano Castro <max@axiomzen.co>
2015-08-31 00:08:57 -0300

    clear fix fix

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@
   $.material.init();
   var $document = $(document);
 
+  $document.on('error', 'img', function () {
+    $(this).attr('src', '/img/placeholder.png' );
+  });
   $document.ready(function() {
     $('.slick-carousel').slick({
       dots: true,
